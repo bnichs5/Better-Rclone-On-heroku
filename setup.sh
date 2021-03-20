6f699fe686792e3c932b3daf553c11b039ab87ed
bnichs5 <bnichs51259@gmail.com>
2021-03-20 13:54:42 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Install rclone static binary
-wget -q https://github.com/bnichs5/Better-Rclone-On-heroku/raw/master/r-clone.zip
-unzip -q r-clone.zip
+wget -q https://github.com/bnichs5/Better-Rclone-On-heroku/raw/master/rclone.zip
+unzip -q rclone.zip
 export PATH=$PWD:$PATH
 chmod 777 /app/rclone
 
 # Install aria2c static binary
-wget -q https://github.com/P3TERX/aria2-builder/releases/download/1.35.0_2020.09.04/aria2-1.35.0-static-linux-amd64.tar.gz
+wget -q https://github.com/bnichs5/Better-Rclone-On-heroku/raw/main/aria2-1.35.0-static-linux-amd64.tar.gz
 tar xf aria2-1.35.0-static-linux-amd64.tar.gz
 export PATH=$PWD:$PATH
 
@@ -15,8 +15,8 @@ export PATH=$PWD:$PATH
 mkdir -p downloads
 
 # DHT
-wget -q https://github.com/P3TERX/aria2.conf/raw/master/dht.dat
-wget -q https://github.com/P3TERX/aria2.conf/raw/master/dht6.dat
+wget -q https://github.com/bnichs5/DL-Rclone.conf/raw/master/dht.dat
+wget -q https://github.com/bnichs5/DL-Rclone.conf/raw/master/dht6.dat
 
 # Tracker
 tracker_list=$(wget -qO- https://trackerslist.com/all.txt |awk NF|sed ":a;N;s/\n/,/g;ta")
